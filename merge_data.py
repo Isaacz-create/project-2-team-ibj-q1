fa57e60162576476155c13fec85f3fd895779fb0
Isaac Zhong <isaaczhong@Isaacs-MacBook-Pro.local>
2025-05-21 23:44:25 -0400

    Fixed the merge bug

--- merge_data.py
+++ merge_data.py
@@ -1,31 +1,25 @@
-
 import requests
 import pandas as pd
 from main import pokemon_names_df
 from pokemon_data import pokemon_info_df
 import sqlite3
 
-# DF1 and DF2 files to merge
-
+# Call the functions to get the DataFrames
 names_df = pokemon_names_df()
-pokemon_info = pokemon_info_df(names_df["Pokemon Names"].tolist())
+info_df = pokemon_info_df(names_df["Pokemon Names"].tolist())
 
+# Merge dataframes using pd.concat
+merged_df = pd.concat([names_df, info_df], axis=1)
 
-#Merge dataframes using pd.concat
-
-merged_df = pd.concat([pokemon_names_df, pokemon_info], axis = 1)
-
-
-# display merged dataframe
+# Display merged dataframe
 print(merged_df.head())
 
-# display descriptive stats
+# Display descriptive stats
 print(merged_df.describe())
 
-#Export to CSV File
-
+# Export to CSV
 merged_df.to_csv('pokemon.csv', index=False)
 
-#Convert DataFrame to SQL
+# Export to SQLite database
 db_conn = sqlite3.connect("pokemon.db")
 merged_df.to_sql("Pokemon_Data", db_conn, if_exists="replace", index=False)
